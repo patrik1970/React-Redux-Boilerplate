8a94cbad062a2c980e47d393963e758e8c14eecc
PPE11208 <patrik.2.persson@sigma.se>
2018-09-15 21:40:40 +0200

    Setting up components and provider

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,9 +1,14 @@
 import 'babel-polyfill';
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
+import {provider} from 'react-redux';
 import {createStore} from 'redux';
 import allRedusers from './reducers';
+import App from './components/app';
 
 const store = createStore(allRedusers);
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(
+    <provider store={store}>
+        <App />
+    </provider>, document.getElementById('root'));
