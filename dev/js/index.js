1b209b3cb7e14a11061d1149a85c10cb4ace56b8
PPE11208 <patrik.2.persson@sigma.se>
2018-09-15 21:10:56 +0200

    Setting up store and redusers

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,5 +1,9 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from 'redux';
+import allRedusers from './reducers';
+
+const store = createStore(allRedusers);
 
 ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
