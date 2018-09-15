f812d8b1c96d14e3f4911687dc7c991062459020
PPE11208 <patrik.2.persson@sigma.se>
2018-09-15 20:34:15 +0200

    Clean working screen

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devServer: {
         inline: true,
         contentBase: './src',
-        port: 3000
+        port: 8000
     },
     devtool: 'cheap-module-eval-source-map',
     entry: './dev/js/index.js',
